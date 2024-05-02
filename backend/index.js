9129ac02ff9fb455b094059865149bd7e3e6707d
Asutosh Dash 9060 <108900267+heh155@users.noreply.github.com>
2024-05-03 03:04:47 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,13 @@ const app = express();
 
 app.use(express.json())
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+));
 
 
 mongoose.connect("mongodb://localhost:27017",{
